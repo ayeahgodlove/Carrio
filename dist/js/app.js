a6923cd78696f5568716cebccec988d2a1ef1c95
Ayeah Godlove <ayeahgodlove5@gmail.com>
2020-07-02 09:22:21 +0100

    ignore this
    ignore this

---
+++ dist/js/app.js
@@ -0,0 +1,9 @@
+// After document has loaded
+window.addEventListener("DOMContentLoaded", event => {
+    setTimeout(() => {
+        document.getElementById('splash').classList.toggle('d-none');
+
+    }, 2000);
+});
+
+// notification example
